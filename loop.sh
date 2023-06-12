2258189710330056ae33c80f969d0355df0a6700
Dan Selby <dan.selby@hud.ac.uk>
2023-06-12 12:33:21 +0100

    Bug fix for diameter of tube

--- loop.sh
+++ loop.sh
@@ -9,14 +9,14 @@ do
 	do	
 		for w in $(seq 0.1 0.1 2.0)
 		do
-			for d in $(seq 3 1 5)
+			for d in $(seq 5 1 11)
 			do
 				turns=1
 				while (( $(echo "(3.14159*$d-$g)/(4*($w+$g)) > $turns" |bc -l) )) && (( $(echo "2*$turns*$w+(2*$turns-1)*$g < $h" |bc -l) ))
 				do
-					mkdir "./$h,$g,$w,5,$turns"
-					cd "./$h,$g,$w,5,$turns"
-					python ~/documents/FastHenryPy/main.py $h $g $w 5 $turns ./FH.inp
+					mkdir "./$h,$g,$w,$d,$turns"
+					cd "./$h,$g,$w,$d,$turns"
+					python ~/documents/FastHenryPy/main.py $h $g $w $d $turns ./FH.inp
 					fasthenry ./FH.inp
 					fasthenry ./FH.inp -f simple
 					zbuf ./zbuf zbuffile -a90 -e80
